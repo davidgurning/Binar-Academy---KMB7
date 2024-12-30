6ea32c6047f3d3d327a26e3c0634bb8991acc214
David Gurning <davidgurning4@gmail.com>
2024-12-30 11:00:38 +0700

    first commit

---
+++ chapter-5/express-js/src/controllers/auth.js
@@ -0,0 +1,26 @@
+const authService = require("../services/auth");
+const { successResponse } = require("../utils/response");
+
+exports.register = async (req, res, next) => {
+  const data = await authService.register(req.body, req.files);
+  successResponse(res, data);
+};
+
+exports.login = async (req, res, next) => {
+  const data = await authService.login(req.body.email, req.body.password);
+  successResponse(res, data);
+};
+
+exports.googleLogin = async (req, res, next) => {
+  const data = await authService.googleLogin(req.body.access_token);
+  successResponse(res, data);
+};
+
+exports.getProfile = async (req, res, next) => {
+  const data = req.user;
+
+  // remove the password object
+  delete data.password;
+
+  successResponse(res, data);
+};
---
+++ chapter-5/express-js/src/routes/auth.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const {
+  validateRegister,
+  validateLogin,
+  authorization,
+  validateGoogleLogin,
+} = require("../middlewares/auth");
+const {
+  register,
+  login,
+  getProfile,
+  googleLogin,
+} = require("../controllers/auth");
+const { adminRole, userRole } = require("../constants/auth");
+
+const router = express.Router();
+
+router.post("/register", validateRegister, register);
+router.post("/login", validateLogin, login);
+router.post("/google/login", validateGoogleLogin, googleLogin);
+router.get("/profile", authorization(adminRole, userRole), getProfile);
+
+module.exports = router;
